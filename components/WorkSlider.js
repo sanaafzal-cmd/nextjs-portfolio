65bda1b659e851980a802b3ca5e41cdfba07a5b3
sanaafzal-cmd <105205204+sanaafzal-cmd@users.noreply.github.com>
2023-08-07 19:04:15 +0500

    7 august

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -65,10 +65,12 @@ const workSlides = {
         {
           title: 'title',
           path: '/thumb2.jpg',
+          link: ''
         },
         {
           title: 'title',
           path: '/thumb3.jpg',
+          link:''
         },
       ],
     },
@@ -92,7 +94,7 @@ const WorkSlider = () => {
            <div className='grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer'>
             {slide.images.map((image, index) => {
              return <div key={index} className='relative rounded-lg overflow-hidden flex items-center justify-center group'>
-              <Link href='/contact'>
+              <Link href={image.link}>
               <div className='flex items-center justify-center relative overflow-hidden group'>
                 {/* Image */}
                 <Image src={image.path} width={500} height={300} alt='' />
